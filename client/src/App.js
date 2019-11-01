cd64ed287ebf7fcb9ff520f94a96faee90cfa131
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-11-01 11:16:13 +0100

    fetching data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [project, setProject] = useState();
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/projects")
+      .then(res => {
+        //console.log(res);
+        setProject(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {project
+        ? project.map(char => (
+            <div key={char.id}>
+              <h4>{char.name}</h4>
+              <p>{char.description}</p>
+              {/* <button onClick={e => removeUser(char.id)}>X</button> */}
+            </div>
+          ))
+        : null}
     </div>
   );
 }
